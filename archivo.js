9bac3f39d50e4daf4c86cb1e388cdced2fe80bb8
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2023-05-16 01:51:57 +0200

    gg

--- archivo.js
+++ archivo.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const audio = document.getElementById("myAudio");
     const audio2 = document.getElementById("myAudio2");
 
-    document.getElementById("bloqueo").style.display = "none";
-
     boton.addEventListener("mouseover", function() {
         boton.style.backgroundColor = "red";
         if (validarFormulario2()) {
